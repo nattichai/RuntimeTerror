ff14d4b730f5c385986d645c3007eeccff49f23c
Nattichai Sutipanwihan <nattichai@hotmail.com>
2019-11-30 16:28:48 +0700

    Add Feature B

--- webapp/services.py
+++ webapp/services.py
@@ -10,20 +10,31 @@ robot_pos = {}
 @app.route("/distance", methods=['POST'])
 def find_distance():
     body = request.get_json()
-    try:
-        first_pos = body['first_pos']
-        second_pos = body['second_pos']
-        print(first_pos, second_pos, type(first_pos), type(second_pos))
-        if type(first_pos) == str and robot_re.fullmatch(first_pos):
-            first_pos = robot_pos[first_pos[6:]]
-        if type(second_pos) == str and robot_re.fullmatch(second_pos):
-            second_pos = robot_pos[second_pos[6:]]
-        return jsonify(distance=_get_euclidean_distance(first_pos, second_pos)), HTTPStatus.OK
-    except:
-        return '', HTTPStatus.BAD_REQUEST
+    # try:
+    first_pos = body['first_pos']
+    second_pos = body['second_pos']
+
+    if type(first_pos) == str and robot_re.fullmatch(first_pos):
+        first_pos = robot_pos[first_pos[6:]]
+
+    if type(second_pos) == str and robot_re.fullmatch(second_pos):
+        second_pos = robot_pos[second_pos[6:]]
+
+    metric = body['metric'] if 'metric' in body else 'euclidean'
+    if metric == 'euclidean':
+        distance = _get_euclidean_distance(first_pos, second_pos)
+    elif metric == 'manhattan':
+        distance = _get_manhattan_distance(first_pos, second_pos)
+
+    return jsonify(distance=distance), HTTPStatus.OK
+    # except:
+    #     return '', HTTPStatus.BAD_REQUEST
 
 def _get_euclidean_distance(p1, p2):
     return ((p1['x'] - p2['x']) ** 2 + (p1['y'] - p2['y']) ** 2) ** 0.5
+
+def _get_manhattan_distance(p1, p2):
+    return abs(p1['x'] - p2['x']) + abs(p1['y'] - p2['y'])
 
 @app.route("/robot/<robot_id>/position", methods=['PUT'])
 def update_robot_position(robot_id):
