339bdba39b5abf919872b40f21f2496d733f488a
Nattichai Sutipanwihan <nattichai@hotmail.com>
2019-11-30 16:40:55 +0700

    Add Feature C

--- webapp/services.py
+++ webapp/services.py
@@ -15,10 +15,10 @@ def find_distance():
         second_pos = body['second_pos']
 
         if type(first_pos) == str and robot_re.fullmatch(first_pos):
-            first_pos = robot_pos[first_pos[6:]]
+            first_pos = robot_pos[int(first_pos[6:])]
 
         if type(second_pos) == str and robot_re.fullmatch(second_pos):
-            second_pos = robot_pos[second_pos[6:]]
+            second_pos = robot_pos[int(second_pos[6:])]
 
         metric = body['metric'] if 'metric' in body else 'euclidean'
         if metric == 'euclidean':
@@ -38,13 +38,31 @@ def _get_manhattan_distance(p1, p2):
 
 @app.route("/robot/<robot_id>/position", methods=['PUT'])
 def update_robot_position(robot_id):
+    robot_id = int(robot_id)
+
     body = request.get_json()
     robot_pos[robot_id] = body['position']
     return '', HTTPStatus.NO_CONTENT
 
 @app.route("/robot/<robot_id>/position", methods=['GET'])
 def get_robot_position(robot_id):
+    robot_id = int(robot_id)
+
     if robot_id in robot_pos:
         return jsonify(position=robot_pos[robot_id]), HTTPStatus.OK
     else:
         return '', HTTPStatus.NOT_FOUND
+
+@app.route("/nearest", methods=['POST'])
+def find_nearest_robot():
+    body = request.get_json()
+
+    ref_position = body['ref_position']
+    nearest_robot = []
+    min_dist = float('inf')
+    for robot_id in robot_pos:
+        dist = _get_euclidean_distance(robot_pos[robot_id], ref_position)
+        if dist < min_dist:
+            min_dist = dist
+            nearest_robot = [robot_id]
+    return jsonify(robot_ids=nearest_robot), HTTPStatus.OK
